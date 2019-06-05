9275cf80d950bb14e60023c37e3778487be2b460
Bruno_M <est.brunomarcelino@gmail.com>
2019-06-05 10:02:50 +0100

    ter

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var app = express();
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
-
-app.listen(8080);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
